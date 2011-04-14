a6be72e63ca98f315ccb16542a04b0b13ecc09da
jamiepratt <me@jamiep.org>
2011-04-14 13:13:16 +0700

    NOBUG copied shortanswer question type as quick start to set up varnumeric question type infrastructure and : * edited install files to set up DB correctly. * placed all occurances of string shortanswer ith varnumeric. * Edited lang file strings that mention shortanswer or are short answer specific. * Changed file names that include shortanswer. * Updated version file.

---
+++ lang/en/qtype_varnumeric.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_varnumeric', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage varnumeric
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addingvarnumeric'] = 'Adding a Variable Numeric Question';
+$string['addmoreanswerblanks'] = 'Blanks for {no} More Answers';
+$string['answer'] = 'Answer: {$a}';
+$string['answermustbegiven'] = 'You must enter an answer if there is a grade or feedback.';
+$string['answerno'] = 'Answer {$a}';
+$string['caseno'] = 'No, case is unimportant';
+$string['casesensitive'] = 'Case sensitivity';
+$string['caseyes'] = 'Yes, case must match';
+$string['correctansweris'] = 'The correct answer is: {$a}.';
+$string['correctanswers'] = 'Correct answers';
+$string['editingvarnumeric'] = 'Editing a Variable Numeric Question';
+$string['filloutoneanswer'] = 'You must provide at least one possible answer. Answers left blank will not be used. \'*\' can be used as a wildcard to match any characters. The first matching answer will be used to determine the score and feedback.';
+$string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
+$string['pleaseenterananswer'] = 'Please enter an answer.';
+$string['varnumeric'] = 'Variable Numeric';
+$string['varnumeric_help'] = 'In response to a question the respondent types a number. Numbers used in the question and used to calculate the answer are chosen from predefined sets or randomly generated on the fly.';
+$string['varnumeric_link'] = 'question/type/varnumeric';
+$string['varnumericsummary'] = 'Allows a numeric response.';
---
+++ simpletest/testquestiontype.php
@@ -0,0 +1,87 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the varnumeric question type class.
+ *
+ * @package    qtype
+ * @subpackage varnumeric
+ * @copyright  2007 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/question/type/varnumeric/questiontype.php');
+
+
+/**
+ * Unit tests for the varnumeric question type class.
+ *
+ * @copyright  2007 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qtype_varnumeric_test extends UnitTestCase {
+    public static $includecoverage = array(
+        'question/type/questiontype.php',
+        'question/type/varnumeric/questiontype.php',
+    );
+
+    protected $qtype;
+
+    public function setUp() {
+        $this->qtype = new qtype_varnumeric();
+    }
+
+    public function tearDown() {
+        $this->qtype = null;
+    }
+
+    protected function get_test_question_data() {
+        $q = new stdClass();
+        $q->id = 1;
+        $q->options->answers[1] = (object) array('answer' => 'frog', 'fraction' => 1);
+        $q->options->answers[2] = (object) array('answer' => '*', 'fraction' => 0.1);
+
+        return $q;
+    }
+
+    public function test_name() {
+        $this->assertEqual($this->qtype->name(), 'varnumeric');
+    }
+
+    public function test_can_analyse_responses() {
+        $this->assertTrue($this->qtype->can_analyse_responses());
+    }
+
+    public function test_get_random_guess_score() {
+        $q = $this->get_test_question_data();
+        $this->assertEqual(0.1, $this->qtype->get_random_guess_score($q));
+    }
+
+    public function test_get_possible_responses() {
+        $q = $this->get_test_question_data();
+
+        $this->assertEqual(array(
+            $q->id => array(
+                1 => new question_possible_response('frog', 1),
+                2 => new question_possible_response('*', 0.1),
+                null => question_possible_response::no_response()),
+        ), $this->qtype->get_possible_responses($q));
+    }
+}

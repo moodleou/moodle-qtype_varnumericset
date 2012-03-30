1bf84f7af56bf70ab8d68e48a7f7e3f1c13a4490
Tim Hunt <T.J.Hunt@open.ac.uk>
2012-03-30 10:35:04 +0100

    Files in hints were not moved or deleted properly for OU qtypes. #3478

--- questiontypebase.php
+++ questiontypebase.php
@@ -70,11 +70,13 @@ abstract class qtype_varnumeric_base extends question_type {
     public function move_files($questionid, $oldcontextid, $newcontextid) {
         parent::move_files($questionid, $oldcontextid, $newcontextid);
         $this->move_files_in_answers($questionid, $oldcontextid, $newcontextid);
+        $this->move_files_in_hints($questionid, $oldcontextid, $newcontextid);
     }
 
     protected function delete_files($questionid, $contextid) {
         parent::delete_files($questionid, $contextid);
         $this->delete_files_in_answers($questionid, $contextid);
+        $this->delete_files_in_hints($questionid, $contextid);
     }
 
     public function delete_question($questionid, $contextid) {
